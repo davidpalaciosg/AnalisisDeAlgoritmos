9541d04956c38627548976d6dbae02dc8ea5b3d8
David Enrique Palacios Garcia <paladavid@hotmail.com>
2022-08-29 15:19:35 -0500

    Naive algorithm fixed

--- Talleres/Taller 3/Code/Python/cadenasMatricialesInocente.py
+++ Talleres/Taller 3/Code/Python/cadenasMatricialesInocente.py
@@ -1,19 +1,19 @@
 import math
 def cadenasMatricialesAux(D,i,j):
-    if i == j:
+    if i==j:
         return 0
-    else:
-        q = math.inf
-        for k in range (i,j):
-            left = cadenasMatricialesAux(D,i,k)
-            right = cadenasMatricialesAux(D,k+1,j)
-            v = left+right+(D[i]*D[k]*D[j])
-            
-            #Hallar el menor           
-            if v < q:
-                q = v
-        return q
+    
+    q = math.inf
+    for k in range (i,j):
+        left = cadenasMatricialesAux(D,i,k)
+        right = cadenasMatricialesAux(D,k+1,j)
+        v = left+right+(D[i]*D[k]*D[j])  
+        #Hallar el menor           
+        if v < q:
+            q = v
+    return q
 
-D = [5,4,6,8,10]
-r = cadenasMatricialesAux(D,0,len(D)-1)
+
+D = [10,30,5,60]
+r = cadenasMatricialesAux(D,0,len(D)-2)
 print(r)
