3de249f20189a1bd42de34af7e5ed13391629973
David Enrique Palacios Garcia <paladavid@hotmail.com>
2022-09-27 15:18:01 -0500

    Prints deleted

--- Talleres/Taller 4/Code/Python/SecuenciaCrecienteMatrizCuadradaInocente.py
+++ Talleres/Taller 4/Code/Python/SecuenciaCrecienteMatrizCuadradaInocente.py
@@ -8,31 +8,19 @@ def secuenciaCrecienteEnMatrizCuadrada(A, i,j):
     #Fila superior
     if i>0:
         if A[i][j]+1 == A[i-1][j]:
-            print("Fila superior")
-            print("A[i][j]", A[i][j])
-            print("A[i-1][j]", A[i-1][j])
             q = 1 + secuenciaCrecienteEnMatrizCuadrada(A, i-1, j)
             
     #Fila inferior
     if i<len(A)-1:
         if A[i][j]+1 == A[i+1][j]:
-            print("Fila inferior")
-            print("A[i][j]", A[i][j])
-            print("A[i+1][j]",A[i+1][j])
             q = 1 + secuenciaCrecienteEnMatrizCuadrada(A, i+1, j)
     #Columna izquierda
     if j>0:
         if A[i][j]+1 == A[i][j-1]:
-            print("Columna izquierda")
-            print("A[i][j]", A[i][j])
-            print("A[i][j-1]",A[i][j-1])
             q = 1 + secuenciaCrecienteEnMatrizCuadrada(A, i, j-1)
     #Columna derecha
     if j<len(A)-1:
         if A[i][j]+1 == A[i][j+1]:
-            print("Columna derecha")
-            print("A[i][j]", A[i][j])
-            print("A[i][j+1]",A[i][j+1])
             q = 1 + secuenciaCrecienteEnMatrizCuadrada(A, i, j+1)
     return q
      
@@ -45,7 +33,6 @@ A = [[10, 16, 15, 12],
 q= -9999999
 for i in range(len(A)):
     for j in range(len(A)):
-        print("i, j", i, j)
         result = secuenciaCrecienteEnMatrizCuadrada(A, i, j)
         if result > q:
             q = result
--- Talleres/Taller 4/Code/Python/SecuenciaCrecienteMatrizCuadradaMem.py
+++ Talleres/Taller 4/Code/Python/SecuenciaCrecienteMatrizCuadradaMem.py
@@ -7,35 +7,23 @@ def secuenciaCrecienteEnMatrizCuadradaMem(A, i,j, M):
     if M[i][j]!=0:
         return M[i][j] 
      
-    q = 0
+    q = 1
     #Fila superior
     if i>0:
         if A[i][j]+1 == A[i-1][j]:
-            print("Fila superior")
-            print("A[i][j]", A[i][j])
-            print("A[i-1][j]", A[i-1][j])
             q = 1 + secuenciaCrecienteEnMatrizCuadradaMem(A, i-1, j, M)
             
     #Fila inferior
     if i<len(A)-1:
         if A[i][j]+1 == A[i+1][j]:
-            print("Fila inferior")
-            print("A[i][j]", A[i][j])
-            print("A[i+1][j]",A[i+1][j])
             q = 1 + secuenciaCrecienteEnMatrizCuadradaMem(A, i+1, j, M)
     #Columna izquierda
     if j>0:
         if A[i][j]+1 == A[i][j-1]:
-            print("Columna izquierda")
-            print("A[i][j]", A[i][j])
-            print("A[i][j-1]",A[i][j-1])
             q = 1 + secuenciaCrecienteEnMatrizCuadradaMem(A, i, j-1, M)
     #Columna derecha
     if j<len(A)-1:
         if A[i][j]+1 == A[i][j+1]:
-            print("Columna derecha")
-            print("A[i][j]", A[i][j])
-            print("A[i][j+1]",A[i][j+1])
             q = 1 + secuenciaCrecienteEnMatrizCuadradaMem(A, i, j+1, M)
     M[i][j] = q
     return M[i][j]
@@ -51,7 +39,6 @@ M = [[0 for i in range(len(A))] for j in range(len(A))]
 
 for i in range(len(A)):
     for j in range(len(A)):
-        print("i, j", i, j)
         result = secuenciaCrecienteEnMatrizCuadradaMem(A, i, j, M)
         if result > q:
             q = result
