5ff280fbdce2882cc030b3abb73fe021440fc87d
David Enrique Palacios Garcia <paladavid@hotmail.com>
2022-09-27 16:13:27 -0500

    Longest sequence with backtrack done in python

---
+++ Talleres/Taller 4/Code/Python/SecuenciaCrecienteMatrizCuadradaBackTrack.py
@@ -0,0 +1,82 @@
+def printMatrix(matrix):
+    for row in matrix:
+        print(row)
+    print()
+
+
+def secuenciaCrecienteEnMatrizCuadradaBacktrack(A, i, j, M, B):
+    if i < 0 or i >= len(A) or j < 0 or j >= len(A):
+        return 0
+
+    if M[i][j] != -1:
+        return M[i][j]
+
+    q = 1
+    sup, inf, izq, der = 0, 0, 0, 0
+    # Fila superior
+    if i > 0:
+        if A[i][j]+1 == A[i-1][j]:
+            sup = 1 + \
+                secuenciaCrecienteEnMatrizCuadradaBacktrack(A, i-1, j, M, B)
+            B[i][j] = (i-1, j)
+
+    # Fila inferior
+    if i < len(A)-1:
+        if A[i][j]+1 == A[i+1][j]:
+            inf = 1 + \
+                secuenciaCrecienteEnMatrizCuadradaBacktrack(A, i+1, j, M, B)
+            B[i][j] = (i+1, j)
+    # Columna izquierda
+    if j > 0:
+        if A[i][j]+1 == A[i][j-1]:
+            izq = 1 + \
+                secuenciaCrecienteEnMatrizCuadradaBacktrack(A, i, j-1, M, B)
+            B[i][j] = (i, j-1)
+    # Columna derecha
+    if j < len(A)-1:
+        if A[i][j]+1 == A[i][j+1]:
+            der = 1 + \
+                secuenciaCrecienteEnMatrizCuadradaBacktrack(A, i, j+1, M, B)
+            B[i][j] = (i, j+1)
+
+    q = max(sup, inf, izq, der)
+    M[i][j] = q
+    return M[i][j]
+
+
+A = [[10, 16, 15, 12],
+     [9, 8, 7, 13],
+     [2, 5, 6, 14],
+     [3, 4, 1, 11]]
+
+def secuenciaCrecienteEnMatriz(A):
+    #Crear tabla de memoizacion y backtracking
+    M = [[-1 for i in range(len(A))] for j in range(len(A))]
+    B = [[(0, 0) for i in range(len(A))] for j in range(len(A))]
+    q = 1
+    # Casos base
+    for i in range(len(A)):
+        for j in range(len(A)):
+            B[i][j] = (i, j)
+
+    max_pos = (0, 0)
+    for i in range(len(A)):
+        for j in range(len(A)):
+            q = max(q, secuenciaCrecienteEnMatrizCuadradaBacktrack(A, i, j, M, B))
+            #Obtener la posicion de la secuencia mas larga
+            if q == M[i][j]:
+                max_pos = (i, j)
+
+    #Backtracking
+    resultado = []
+    #Mientras no llegue a la posicion inicial
+    while max_pos != B[max_pos[0]][max_pos[1]]:
+        resultado.append(A[max_pos[0]][max_pos[1]])
+        max_pos = B[max_pos[0]][max_pos[1]]
+        
+    #Agregar el ultimo elemento
+    resultado.append(A[max_pos[0]][max_pos[1]])
+    
+    return resultado
+
+print(secuenciaCrecienteEnMatriz(A))
